ed33d9562c199658bff864cd2145b0a42339624f
Spark1er <ricksanford498@gmail.com>
2018-04-29 17:15:14 +0700

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,34 @@ import { AppComponent } from "./app.component";
 import { PostsComponent } from "./posts/posts.component";
 import { PostService } from "./services/post.service";
 import { DataService } from "./services/data.service";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { GithubProfileComponent } from "./github-profile/github-profile.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { HomeComponent } from "./home/home.component";
+import { RouterModule } from "@angular/router";
+import { GithubFollowersComponent } from "./github-followers/github-followers.component";
 
 @NgModule({
-  declarations: [AppComponent, PostsComponent],
-  imports: [BrowserModule, HttpModule],
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    NavbarComponent,
+    GithubProfileComponent,
+    NotFoundComponent,
+    HomeComponent,
+    GithubFollowersComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: "", component: HomeComponent },
+      { path: "follower/:username", component: GithubProfileComponent },
+      { path: "followers", component: GithubFollowersComponent },
+      { path: "posts", component: PostsComponent },
+      { path: "**", component: NotFoundComponent }
+    ])
+  ],
   providers: [PostService, DataService],
   bootstrap: [AppComponent]
 })
